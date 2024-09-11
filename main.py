5b0fbfc1d0aef4c5934fd02a2b9288476c0d8871
Madison Berti <maddieberti@gmail.com>
2024-09-10 18:51:47 -0700

    Start of project with frame for main window, rough draft of planting timelines, checkboxes for opt in/out of reminders.

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
+#GardenReminders
+import tkinter as tk
+from tkinter import ttk
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+root =  tk.Tk()
+#Set the title displayed on the window
+root.title("Garden Reminders")
+#Set the size of the window
+root.geometry("1200x600")
 
+#List of plants
+plants = ["Potatoes", "Garlic", "Tomatoes", "Peppers", "Cilantro", "Carrots", "Cucumber", "Broccoli", "Blackberries"]
+#Frame/container for selecting plants
+plant_frame = tk.LabelFrame(root, text="Select Plants to Grow", padx=10, pady=10)
+plant_frame.pack(padx=10, pady=10, fill="both")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+#Dictionary that holds state of check boxes/selection status of plants
+plant_vars = {}
 
+#Loop to make checkboxes for every plant
+for plant in plants:
+    #variable to track each checkbox state
+    var = tk.BooleanVar()
+    checkbox = tk.Checkbutton(plant_frame, text=plant, variable=var)
+    #Position checkboxes to the left of the window
+    checkbox.pack(anchor='w')
+    #store plant name and selection status in the dictionary
+    plant_vars[plant] = var
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#Container for more plant info such as timelines, harvest dates etc
+timeline_frame = tk.LabelFrame(root, text="Planting Timelines", padx=5, pady=5)
+timeline_frame.pack(padx=5, pady=5, fill="both")
+# Make timeline frame fit any size window
+timeline_frame.grid_rowconfigure(0, weight=1)
+timeline_frame.grid_columnconfigure(0, weight=1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# add a treeview widget and define the headings for each process
+timeline_tree = ttk.Treeview(timeline_frame, columns=("Plant", "Start", "Sow", "Transplant", "Harvest", "Fertilize", "Prune"), show="headings")
+timeline_tree.pack(fill="both", expand=True)
+#headings
+timeline_tree.heading("Plant", text="Plant Name")
+timeline_tree.heading("Start", text="Start Indoors")
+timeline_tree.heading("Sow", text="Sow")
+timeline_tree.heading("Transplant", text="Transplant")
+timeline_tree.heading("Harvest", text="Harvest")
+timeline_tree.heading("Fertilize", text="Fertilize")
+timeline_tree.heading("Prune", text="Prune")
+
+#define plant data for each plant
+# order: plant name, start indoors, sow, transplant, harvest, fertilize, prune
+# this information is sort of a placeholder as of Sept 10 (format Month-Month). Will add more specific dates later
+# after doing more research on DateTime.
+plant_data = [
+    ("Tomatoes", "Feb-Apr", "Apr-Jun", "Apr-Jun", "Jul-Sep", "", ""),
+    ("Potatoes", "", "Jan-Mar", "", "Jun-Jul", "", ""),
+    ("Garlic", "", "Sept-Nov", "", "Jul-Aug", "", ""),
+    ("Broccoli", "Feb-Mar", "Mar-Apr", "Mar-Apr", "May-Jun", "", ""),
+    ("Peppers", "Feb-Mar", "Apr-May", "Apr-May", "Jul-Nov", "", ""),
+    ("Cilantro", "", "Apr-May", "", "Jun-Sept", "", ""),
+    ("Carrots", "", "Mar-Apr", "", "Jun-Jul", "", ""),
+    ("Cucumber", "Mar-Apr", "Apr-Jun", "Apr-Jul", "Aug-Sept", "", ""),
+    ("Blackberries", "", "Feb-Mar", "", "Jun-Jul", "Feb-Mar", "Feb-Mar")
+
+]
+#populate plant data into treeview widget
+for item in plant_data:
+    timeline_tree.insert("", "end", values=item)
+
+root.mainloop()
